ab068ea0fbc4930eededa9d68cde2e98d59cecdd
Jacqueline <jacqueline_grimaldo1@baylor.edu>
2024-01-24 14:41:21 -0600

    updated gitignore and print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,4 +6,11 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+
+
+#these lines of code will be on the quiz 
+#1st step: create a VE for mac it is python3 -m venv myvenv
+#2nd step: Activate the VE for mac it is source myvenv/bin/activate 
+#3rd step: Install 3rd party library/module and the name is pip3 install matplotlib
+# there is also pip 3 uninstall which removes the library 
